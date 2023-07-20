2c46564de23d3190140685128ba3cad7a3753c5f
neoats <fuatatessaca@gmail.com>
2023-07-20 22:26:50 +0300

    1

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,15 @@
-import React from 'react'
-import {getPostDetails, getPosts,getPostsList} from '../../services';
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components';
+import React from 'react';
+import { useRouter } from 'next/router';
+
+import {getPostDetails, getPosts} from '../../services';
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader} from '../../components';
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
@@ -14,7 +22,7 @@ const PostDetails = ({ post }) => {
             </div>
             <div className='col-span-1 lg:col-span-4'>
                  <div className='relative lg:sticky top-8'>
-                    <PostWidget slug={post.slug}/>
+                 <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
                     <Categories/>
                 </div>
             </div>
@@ -22,8 +30,6 @@ const PostDetails = ({ post }) => {
     </div>
   )
 }
-
-export default PostDetails
 
 export async function getStaticProps({params}) {
 const data = await getPostDetails(params.slug);
@@ -39,3 +45,4 @@ return{
       fallback: true,
     };
   }
+export default PostDetails
